3d8b9f8c860fb92456b9b37a6897e40b1b4725d6
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2023-02-10 14:32:15 -0800

    Add files via upload

--- downloads_gallary/downloads_gallery.js
+++ downloads_gallary/downloads_gallery.js
@@ -1,6 +1,52 @@
-$(".renderlink").sort(sort_li) // sort elements
-                  .appendTo('.renderlink'); // append again to the list
-// sort function callback
-function sort_li(a, b){
-    return ($(b).data('date')) < ($(a).data('date')) ? 1 : -1;    
+
+//comparison for newest first mode
+function comparatornewfirst(a, b) {
+    if (a.dataset.date > b.dataset.date)
+        return -1;
+    if (a.dataset.date < b.dataset.date)
+        return 1;
+    return 0;
 }
+
+//comparison for olest first mode
+function comparatoroldfirst(a, b) {
+    if (a.dataset.date < b.dataset.date)
+        return -1;
+    if (a.dataset.date > b.dataset.date)
+        return 1;
+    return 0;
+}
+
+var sortmode = "pageload";
+
+// Function to sort Data
+function SortData() {
+    console.log("-----function beginning-----")
+    console.log(sortmode)
+    if (sortmode == "pageload" || "oldest") {
+        //code to change sortmode to newest
+
+        var indexes = document.querySelectorAll("[data-date]");
+        var indexesArray = Array.from(indexes);
+        let sorted = indexesArray.sort(comparatornewfirst);
+        sorted.forEach(e =>
+            document.querySelector(".imagecontainer").appendChild(e));
+        console.log("test")
+        sortmode = "newest";
+    } else if (sortmode == "newest") {
+        //code to change sortmode to oldest first
+
+        var indexes = document.querySelectorAll("[data-date]");
+        var indexesArray = Array.from(indexes);
+        let sorted = indexesArray.sort(comparatoroldfirst);
+        sorted.forEach(e =>
+            document.querySelector(".imagecontainer").appendChild(e));
+        sortmode = "oldest";
+        console.log("test2")
+    }
+    console.log(sortmode)
+    console.log("function end")
+
+
+}
+

874b0733d107c15979def643b4ca1cadc563263c
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2023-02-14 14:46:58 -0800

    Add files via upload

--- downloads_gallary/downloads_gallery.js
+++ downloads_gallary/downloads_gallery.js
@@ -17,25 +17,45 @@ function comparatoroldfirst(a, b) {
     return 0;
 }
 
+//setting the default sortmode on pageload to be pageload so that when it runs "sortdata()"
+//function it will switch to newest first without hiccups.
 var sortmode = "pageload";
 
 // Function to sort Data
 function SortData() {
+
+    //tests
     console.log("-----function beginning-----")
     console.log(sortmode)
+
+
     if (sortmode == "pageload" || "oldest") {
         //code to change sortmode to newest
 
+
+        document.getElementById("sortbutton").innerHTML = "Sort: Newest";
+
+        //setting a variable that contains all of the image elements
         var indexes = document.querySelectorAll("[data-date]");
+        //setting the prior variable to be an array
         var indexesArray = Array.from(indexes);
+        //sorting array based on newest first
         let sorted = indexesArray.sort(comparatornewfirst);
+        //appending sorted list back into original elements
         sorted.forEach(e =>
             document.querySelector(".imagecontainer").appendChild(e));
+        
+        //tests
         console.log("test")
+        //setting sortmode to newest so that if you click the button again it will go to oldest first
         sortmode = "newest";
     } else if (sortmode == "newest") {
         //code to change sortmode to oldest first
+        //basically all the same code as above but sorts oldest first (there is probably a much more
+        //efficiant way of doing this).
 
+        document.getElementById("sortbutton").innerHTML = "Sort: Oldest";
+        
         var indexes = document.querySelectorAll("[data-date]");
         var indexesArray = Array.from(indexes);
         let sorted = indexesArray.sort(comparatoroldfirst);
@@ -44,6 +64,8 @@ function SortData() {
         sortmode = "oldest";
         console.log("test2")
     }
+
+    //other tests
     console.log(sortmode)
     console.log("function end")
 

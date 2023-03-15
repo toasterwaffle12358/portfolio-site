503ed3154b4649d22c7c0f8d558eec30d8ce0af8
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2023-03-15 14:24:21 -0700

    various improvements
    currently opening images in downloads page is glitchy

--- downloads_gallary/downloads_gallery.js
+++ downloads_gallary/downloads_gallery.js
@@ -72,49 +72,35 @@ function SortData() {
 
 }
 
+//setting all high res renders from css 0% to js "0%", doing this because later function checks if images are
+// equal to "0%", but because they are not technecally equal to "0%" it needs user to click twice the first time
+rendershighres = document.getElementsByClassName("render_highres")
+for (var i = 0; i < rendershighres.length; i++) {
+    rendershighres[i].style.height = "0%";
+}
+
+var iscurrentimgopen = false
 var lastonimg = "none"
+var lastoninfo = "none"
+
 
 function ShowHighRes(highresid,infoid) {
     var highresimg = document.getElementById(highresid);
-    var infoelemenet = document.getElementById(infoid)
+    var infoelemenet = document.getElementById(infoid);
 
-    if (highresimg.style.height == "0%") {
-        if (lastonimg != "none") {
-            lastonimg.style.display = "none";
-            console.log(lastonimg)
-        }
+    if (iscurrentimgopen == false) {
+        iscurrentimgopen = true
         if (highresimg.naturalWidth <= highresimg.naturalHeight) {
-            highresimg.style.display = "inline";
-            highresimg.style.height = "250%";
-            highresimg.style.border = "solid 5px";
-            highresimg.style.borderColor =  "#2b2b2b";
-            var imgratio = highresimg.naturalWidth/highresimg.naturalHeight
-            var infowidth = imgratio*400
-            var infowidthstring = infowidth.toString().concat("%")
-            console.log(infowidthstring)
-            setTimeout(() => {
-                infoelemenet.style.width = infowidthstring
-                infoelemenet.style.border = "solid 5px";
-                infoelemenet.style.borderColor =  "#2b2b2b";
-                infoelemenet.style.paddingRight = "5px";
-            }, 500);
+            openimgtall(highresimg,infoelemenet,lastoninfo,lastonimg)
         } else {
-            highresimg.style.display = "inline";
-            highresimg.style.width = "250%";
-            highresimg.style.border = "solid 5px";
-            infoelemenet.style.border = "solid 5px";
-            infoelemenet.style.borderColor =  "#2b2b2b";
-            infoelemenet.style.paddingRight = "5px";
-            highresimg.style.borderColor =  "#2b2b2b";
-            infoelemenet.style.width = "300%";
+            openimglong(highresimg,infoelemenet,lastoninfo,lastonimg)
         }
-
-
 
         lastonimg = highresimg
+        lastoninfo = infoelemenet
 
     } else {
-        lastonid = "none"
+        lastonimg = "none"
         infoelemenet.style.width = "0%";
         infoelemenet.style.border = "solid 0px";
         infoelemenet.style.paddingRight = "0px";
@@ -124,6 +110,84 @@ function ShowHighRes(highresid,infoid) {
             highresimg.style.border = "solid 0px";
             highresimg.style.borderColor =  "#2b2b2b";
         }, 500);
+        iscurrentimgopen = false
 
     }
+}
+function openimgtall(highresimg,infoelemenet,lastoninfo,lastonimg) {
+    //opening highresimg
+    highresimg.style = `
+        height: 250%;
+        border: solid 5px;
+        border-color: #2b2b2b;
+        display: inline;
+    `
+    //getting % of parent that highresimg width takes up
+    var imgratio = highresimg.naturalWidth/highresimg.naturalHeight
+    var infowidth = imgratio*400
+    var infowidthstring = infowidth.toString().concat("%")
+    //opening info element
+    setTimeout(() => {
+        infoelemenet.style = `
+            border: solid 5px;
+            border-color: #2b2b2b;
+            padding-right: 5px;
+        `
+        infoelemenet.style.width = infowidthstring
+    }, 500);
+    
+    //getting rid of last img and info element
+    lastoninfo.style = `
+        width: 0%
+        border: solid 0px;
+        padding-right: 0px;
+        border-color: #2b2b2b;
+    `
+    setTimeout(() => { 
+        lastonimg.style = `
+            height: 0%;
+            border: solid 0px;
+            border-color: #2b2b2b;
+        `
+        lastonimg = "none"
+    }, 500);
+    lastoninfo = "none"
+}
+
+function openimglong(highresimg,infoelemenet,lastoninfo,lastonimg) {
+    //opening highresimg
+    var imgratio = highresimg.naturalHeight/highresimg.naturalWidth
+    var imgratiostring = imgratio * 250
+    var imgheightstring = imgratiostring.toString().concat("%")
+    highresimg.style = `
+        border: solid 5px;
+        border-color: #2b2b2b;
+        display: inline;
+        height: ${imgheightstring}
+    `
+    setTimeout(() => {
+        infoelemenet.style.width = "300%";
+        infoelemenet.style.height = imgheightstring;
+        infoelemenet.style.border = "solid 5px";
+        infoelemenet.style.paddingRight = "5px";
+        infoelemenet.style.borderColor =  "#2b2b2b";
+    }, 500);
+    
+    //getting rid of last img and info element
+    lastoninfo.style = `
+        width: 0%;
+        border: solid 0px;
+        padding-right: 0px;
+        border-color: #2b2b2b;
+    `
+    setTimeout(() => { 
+        lastonimg.style = `
+            height: 0%;
+            border: solid 0px;
+            border-color: #2b2b2b;
+        `
+        lastonimg = "none"
+    }, 500);
+    lastoninfo = "none"
+
 }
